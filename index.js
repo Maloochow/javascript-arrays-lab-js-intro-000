7d6a99d77a299c71da4253daf7dd80036de6c786
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-09 00:03:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,3 +57,11 @@ function appendKitten(a) {
 function prependKitten(a) {
   return [a, ...kittens];
 }
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
